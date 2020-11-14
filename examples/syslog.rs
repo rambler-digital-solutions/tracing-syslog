828a296538796ed80abbecd8997b7f2a66dafcf4
Andrey Ermilov <andrerm@ya.ru>
2020-11-14 19:34:08 +0300

    Update example

--- examples/syslog.rs
+++ examples/syslog.rs
@@ -1,11 +1,12 @@
 use tracing_subscriber::prelude::*;
 use tracing_syslog;
 use tracing::{event, Level};
+use tracing_syslog::{Formatter3164, ELKMessage};
 
 fn main() {
     let registry = tracing_subscriber::registry()
         .with(tracing_subscriber::fmt::subscriber().with_target(false));
-    let subscriber = tracing_syslog::SubscriberBuilder::default().finish();
+    let subscriber = tracing_syslog::Subscriber::<Formatter3164, ELKMessage>::builder().finish();
     registry.with(subscriber).init();
     event!(Level::INFO, key_one = "value_one", key_two = "value_two");
 }
